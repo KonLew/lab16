719f4dfdcc42cc66640692d0c35cc8fe0b2ae38a
KonLew <surangrat231@gmail.com>
2023-02-07 16:24:24 +0700

    lab16_1 com

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,25 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+    int *x = &a;
+    char *y = &b;
+    int **z = &x;
+    char i = ' ';
+    
+    cout << a << i << b << i << c << i << x << i << (int *)y << i << z << '\n';
+    cout << &a << i << (int *)&b << i << (int *)&c << i << &x << i << &y << i << &z << '\n';
+    c = 'F';
+    cout << a << i << b << i << c << i << x << i << (int *)y << i << z << '\n';
+    *y = 'W';
+    cout << a << i << b << i << c << i << x << i << (int *)y << i << z << '\n';
+    *x = 6;
+    cout << a << i << b << i << c << i << x << i << (int *)y << i << z << '\n';
+    **z = 7;
+    cout << a << i << b << i << c << i << x << i << (int *)y << i << z << '\n';
 	
 	return 0;
 }
